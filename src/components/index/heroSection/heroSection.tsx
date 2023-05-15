be939a1376b112ccf1e6b23b887b4bc499872571
francescopalomba <palomba.fra@gmail.com>
2023-05-15 16:04:10 +0200

    updates

--- src/components/index/heroSection/heroSection.tsx
+++ src/components/index/heroSection/heroSection.tsx
@@ -56,38 +56,39 @@ export const HeroSection = () => {
                 </div>
             </div>
 
-            <div className={splineContainer}>
+            <div className="hidden md:block">
+                <div className={splineContainer}>
 
 
-                <AnimatePresence>
-                    {
-                        !loaded &&
-                        <motion.div className={loader} exit={{opacity: 0}}>
-                            <img src={'/images/lazy-cubes.png'}/>
+                    <AnimatePresence>
+                        {
+                            !loaded &&
+                            <motion.div className={loader} exit={{opacity: 0}}>
+                                <img src={'/images/lazy-cubes.png'}/>
 
-                            <svg className="animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                        strokeWidth="4"></circle>
-                                <path className="opacity-75" fill="currentColor"
-                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
-                        </motion.div>
-                    }
-                </AnimatePresence>
+                                <svg className="animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                            strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor"
+                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                            </motion.div>
+                        }
+                    </AnimatePresence>
 
 
-                <motion.div initial={{opacity: 0}} animate={loaded && {opacity: 1}} className="cursor-grab active:cursor-grabbing">
-                    {
-                        Spline &&
-                        <Spline onLoad={() => setLoaded(true)}
-                                scene="https://prod.spline.design/pobLkzB1W8D2qzId/scene.splinecode"/>
+                    <motion.div initial={{opacity: 0}} animate={loaded && {opacity: 1}} className="cursor-grab active:cursor-grabbing">
+                        {
+                            Spline &&
+                            <Spline onLoad={() => setLoaded(true)}
+                                    scene="https://prod.spline.design/pobLkzB1W8D2qzId/scene.splinecode"/>
 
-                    }
-                </motion.div>
+                        }
+                    </motion.div>
 
+                </div>
             </div>
-
         </>
     )
 }
