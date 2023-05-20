58a56b2adf1f829f1843c2fd59d79bd40bd259c3
francescopalomba <palomba.fra@gmail.com>
2023-05-20 20:37:46 +0200

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
-import { HeroSection } from "../src/components/index/hero-section/heroSection";
-import { Navbar } from "../src/components/_navbar/navbar";
-import { CareerSection } from "../src/components/index/career-section/career-section";
-import { CubeSVG } from "../src/SVGs/cube";
+import { HeroSection } from '../src/components/index/hero-section/heroSection'
+import { Navbar } from '../src/components/_navbar/navbar'
+import { CareerSection } from '../src/components/index/career-section/career-section'
+import { CubeSVG } from '../src/SVGs/cube'
+import { CogIcon } from '@heroicons/react/outline'
 
 export default function Home() {
 
     return (
         <>
 
-            <Navbar/>
+            <Navbar />
 
-            <CubeSVG className="absolute -left-20 top-[700px] -rotate-12 opacity-60"/>
+            <CubeSVG className='absolute -left-20 top-[700px] -rotate-12 opacity-60' />
 
-            <main className="container mt-60 border-s-2 border-white tracking-wide dark:border-white/10 md:mt-72">
+            <main className='container mt-60 border-s-2 border-white tracking-wide dark:border-white/10 md:mt-72'>
 
-                <HeroSection/>
+                <HeroSection />
 
-                <CareerSection/>
+                <CareerSection />
 
             </main>
+
+            <h5 className='my-10 text-center opacity-40'>
+                <CogIcon className='mx-auto animate-[spin_5s_linear_infinite]' width={20} />
+                Website under construction
+            </h5>
 
         </>
     )
